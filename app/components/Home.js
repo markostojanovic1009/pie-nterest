5a603dbb7c5409d67b43de9e0c9fd8d7c95f2542
DeusExMachina <deusxmachina96@gmail.com>
2016-08-03 18:00:47 +0200

    Init commit with user authentication

---
+++ app/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Messages from './Messages';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="expanded row">
+        <Messages messages={this.props.messages}/>
+        <div className="row">
+          <div className="small-12 columns">
+            <h1 className="intro-text">
+              Welcome to PinterestClone! Like Pinterest. Only worse!
+            </h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    messages: state.messages
+  };
+};
+
+export default connect(mapStateToProps)(Home);
